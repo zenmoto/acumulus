b133f7c1b125b853149e46d721c242f91f20128c
David Poncelow <david@balrog.org>
2013-09-12 00:56:25 -0700

    Have add, sum, series going.
    Still need to add removal of samples.

--- acumulus.js
+++ acumulus.js
@@ -27,30 +27,81 @@
 
     function acumulus() {
         var obj = {};
+        var current = {};
+        var data = [current];
+        obj.length = data.length;
+        var interval_num;
+
         var settings = {
-            sample_interval: 1000
+            sample_interval: 1000,
+            max_samples: 60
         };
 
-        function get_or_set(key, value) {
+        function update() {
+            current = {};
+            data.push(current);
+            obj.length = data.length;
+        }
+
+        function start() {
+            interval_num = setInterval(update, settings.sample_interval);
+        }
+
+        function stop() {
+            clearInterval(interval_num);
+        }
+
+        function restart() {
+            data = []; // We may have changed intervals, data may be invalid.  Start over.
+            current = {};
+            data.push(current);
+            stop();
+            start();
+        }
+
+        function get_value(obj, name) {
+            return obj.hasOwnProperty(name) ? obj[name] : 0;
+        }
+        function add(one, two) {
+            return one + two;
+        }
+
+        function sum(ary) {
+            return ary.reduce(add, 0);
+        }
+
+        obj.sample_interval = function(value) {
             if (value == undefined) {
-                return settings[key];
+                return settings.sample_interval;
             } else {
-                if (settings.hasOwnProperty(key)) {
-                    settings[key] = value;
-                    return obj;
-                }
+                settings.sample_interval = value;
+                restart();
+                return obj;
             }
         }
 
-        Object.keys(settings).forEach(function(setting_name) {
-            obj[setting_name] = function(val) {
-                return get_or_set(setting_name, val);
-            }
-        })
-
         obj.add = function (key, value) {
-
+            current[key] = (current[key] || 0) + (typeof value === 'number' ? value : 1);
+            return current[key];
         };
+
+        obj.get = function(key) {
+            return get_value(current, key);
+        };
+
+        obj.series = function(key) {
+            // TODO: Figure out how to factor out this function creation
+            return data.map(function(e) {
+                return get_value(e, key);
+            })
+        };
+
+        obj.sum = function(key) {
+            return sum(obj.series(key));
+        };
+
+
+        start();
 
         return obj;
     }
--- test/test.js
+++ test/test.js
@@ -1,13 +1,62 @@
 var expect = require('chai').expect;
 var acumulus = require('../acumulus');
 
-describe('acumulus()', function () {
+describe('acumulus', function () {
     it('is defined', function () {
         expect(acumulus).to.be.a('function');
         var a = acumulus();
         expect(a).to.be.an('object');
         expect(a.sample_interval).to.be.a('function');
-        expect(a.sample_interval(750)).to.be.an('object');
-        expect(a.sample_interval()).to.equal(750);
+        expect(a.sample_interval(50)).to.be.an('object');
+        expect(a.sample_interval()).to.equal(50);
     });
+
+    it('has a length', function(done) {
+        var a = acumulus().sample_interval(10);
+        expect(a.length).to.equal(1);
+        setTimeout(function() {
+            expect(a.length).to.equal(2);
+            done();
+        }, a.sample_interval() + 5);
+    });
+
+    it("allows accumulation and retreival by key", function() {
+        var a = acumulus();
+        expect(a.add("foo", 5)).to.equal(5);
+        expect(a.add("foo")).to.equal(6);
+        expect(a.get("foo")).to.equal(6);
+    });
+
+    it("allows retreival of a series of values", function(done) {
+        var a = acumulus().sample_interval(10);
+        a.add("foo");
+        setTimeout(function() {
+            a.add("foo", 3);
+            expect(a.series("foo")).to.eql([1,3]);
+            done();
+        }, 15);
+    });
+
+    it("allows retrieval of sparse values (those which didn't receive an update during a tick)", function(done) {
+        var a = acumulus().sample_interval(10);
+        a.add("foo");
+        setTimeout(function() {
+            a.add("foo");
+            expect(a.series("foo")).to.eql([1, 0, 1]);
+            done();
+        }, 25);
+    });
+
+    it("allows for retrieval of a sum across all stored values", function(done) {
+        var a = acumulus().sample_interval(10);
+        a.add("foo");
+        setTimeout(function() {
+            a.add("foo");
+            expect(a.sum("foo")).to.equal(2);
+            expect(a.length).to.equal(3);
+            done();
+        }, 25);
+    });
+
+
 });
